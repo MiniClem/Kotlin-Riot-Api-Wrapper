8bbbc56d02dd3365de2185e8402e22f4eb3c5c8d
Clem <cmonesti@gmail.com>
2020-07-22 23:49:29 +0200

    Routing values

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -2,9 +2,17 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import services.ClientApi
+import services.ParamProperties
+import services.PlatformRoutes
+import services.RegionalRoutes
 import services.endpoints.ChampionInfo
 
 fun main() {
+    ClientApi.apply {
+//        setPlatformRoute(PlatformRoutes.EUW1)
+//        setRegionalRoute(RegionalRoutes.EUROPE)
+        setToken(ParamProperties())
+    }
     ClientApi.championV3.getChampionRotationsSync().enqueue(object : Callback<ChampionInfo> {
         override fun onFailure(call: Call<ChampionInfo>, t: Throwable) {
             t.printStackTrace()
@@ -14,5 +22,4 @@ fun main() {
             println(response)
         }
     })
-    println("yolo")
 }
--- src/main/kotlin/services/ClientApi.kt
+++ src/main/kotlin/services/ClientApi.kt
@@ -7,17 +7,47 @@ import services.endpoints.*
 
 object ClientApi {
 
+    private lateinit var interceptorInterface: InterceptorInterface
+    private var platformRoutes: PlatformRoutes = PlatformRoutes.EUW1
+    private var regionalRoutes: RegionalRoutes = RegionalRoutes.EUROPE
+
+    /**
+     * You must provide this interface to the client service to use any endpoint
+     */
+    fun setToken(interceptorInterface: InterceptorInterface): ClientApi {
+        this.interceptorInterface = interceptorInterface
+        return this
+    }
+
+    /**
+     * Set the route to be used with the endpoints using the platform route parameter
+     * Default is [PlatformRoutes.EUW1]
+     */
+    fun setPlatformRoute(platformRoutes: PlatformRoutes): ClientApi {
+        this.platformRoutes = platformRoutes
+        return this
+    }
+
+    /**
+     * Set the route to be used with the endpoints using the platform route parameter
+     * Default is [PlatformRoutes.EUW1]
+     */
+    fun setRegionalRoute(regionalRoutes: RegionalRoutes): ClientApi {
+        this.regionalRoutes = regionalRoutes
+        return this
+    }
+
     private val retrofit: Retrofit by lazy {
         val client = OkHttpClient.Builder()
             .apply {
-                interceptors().add(HeaderInterceptor(ParamProperties()))
+                interceptors().add(HeaderInterceptor(interceptorInterface))
             }.build()
 
 
 
         Retrofit.Builder()
             .client(client)
-            .baseUrl("https://euw1.api.riotgames.com")
+            .baseUrl("https://${platformRoutes.name}")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -41,4 +71,24 @@ object ClientApi {
     val tournamentV4: TournamentV4 = retrofit.create(TournamentV4::class.java)
     val valContentV1: ValContentV1 = retrofit.create(ValContentV1::class.java)
     val valMatchV1: ValMatchV1 = retrofit.create(ValMatchV1::class.java)
+}
+
+enum class PlatformRoutes(val route: String) {
+    BR1("br1.api.riotgames.com"),
+    EUN1("eun1.api.riotgames.com"),
+    EUW1("euw1.api.riotgames.com"),
+    JP1("jp1.api.riotgames.com"),
+    KR("kr.api.riotgames.com"),
+    LA1("la1.api.riotgames.com"),
+    LA2("la2.api.riotgames.com"),
+    NA1("na1.api.riotgames.com"),
+    OC1("oc1.api.riotgames.com"),
+    TR1("tr1.api.riotgames.com"),
+    RU("ru.api.riotgames.com"),
+}
+
+enum class RegionalRoutes(val route: String) {
+    AMERICAS("americas.api.riotgames.com"),
+    ASIA("asia.api.riotgames.com"),
+    EUROPE("europe.api.riotgames.com")
 }
