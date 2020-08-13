d4aa55d4ce2be7de724d36d61f4f73862b2fbe41
Clem <cmonesti@gmail.com>
2020-08-13 22:20:57 +0200

    Kotlin example

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,14 +1,24 @@
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import services.*
+import services.ClientApi
+import services.ParamProperties
+import services.PlatformRoutes
 import services.endpoints.ChampionInfo
 
 fun main() {
     ClientApi.apply {
+        // You must provide your API key,
         tokenProvider = ParamProperties()
     }
+    // OR simply put
+    /*ClientApi.apply {
+        tokenProvider = object : TokenProvider {
+            override fun getToken(): String {
+                return "YOUR API KEY"
+            }
+        }
+    }*/
 
     ClientApi.championV3(PlatformRoutes.EUW1).getChampionRotations().enqueue(object : Callback<ChampionInfo> {
         override fun onFailure(call: Call<ChampionInfo>, t: Throwable) {
